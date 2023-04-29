3ec0b2f1fb9b5857bb23eda0c90265315467b1dc
shiva508 <dasarishiva1@gmail.com>
2023-04-29 11:10:52 +0530

    Changes

---
+++ behavioral/strategy/readme.md
@@ -0,0 +1,2 @@
+# Strategy
+### This pattern is used to create different algorithms on runtime when object is created
--- behavioral/strategy/src/main/java/com/pool/Main.java
+++ behavioral/strategy/src/main/java/com/pool/Main.java
@@ -1,7 +1,15 @@
 package com.pool;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Integer> numbers= Arrays.asList(508,28,48,26,408,27,3,501);
+        var collect = numbers.stream().sorted(Comparator.reverseOrder()).collect(Collectors.groupingBy(num -> num % 2 == 0));
+        System.out.println(collect);
     }
 }
